06b3cef908fef0b717cb1cd651b9b2ed015b330b
lgklsv <kolosov20011@gmail.com>
2023-06-15 22:58:53 +0300

    feat: implement list function that prints folder filenames to console

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import * as url from 'url';
+import fs from 'fs/promises';
+import path from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const folderToRead = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await fs.access(folderToRead, fs.F_OK);
+    const files = await fs.readdir(folderToRead);
+    files.forEach(file => console.log(file));
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
