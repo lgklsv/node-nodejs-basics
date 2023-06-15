a142ef2f96ecef3c48ec907c0d32b541d6b19449
lgklsv <kolosov20011@gmail.com>
2023-06-15 23:02:10 +0300

    feat: add print file content function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import * as url from 'url';
+import fs from 'fs/promises';
+import path from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    await fs.access(fileToRead, fs.F_OK);
+    const fileContent = await fs.readFile(fileToRead, 'utf-8');
+    console.log(fileContent);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
