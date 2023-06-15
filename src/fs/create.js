f92f813ec46ecb10dba04d03f182636a7bad1f83
lgklsv <kolosov20011@gmail.com>
2023-06-15 18:30:30 +0300

    feat: implement create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import * as url from 'url';
+import fs from 'fs';
+import path from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+  fs.access(filePath, fs.F_OK, err => {
+    if (err) return;
+
+    //file exists
+    throw new Error('FS operation failed');
+  });
+
+  fs.appendFile(filePath, 'I am fresh and young', function (err) {
+    if (err) throw new Error('FS operation failed');
+    console.log('Saved!');
+  });
 };
 
 await create();
