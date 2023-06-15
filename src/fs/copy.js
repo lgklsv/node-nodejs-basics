15d3f76188fa5520576a2375fca63b7f184876c4
lgklsv <kolosov20011@gmail.com>
2023-06-15 22:26:19 +0300

    feat: create copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import * as url from 'url';
+import fs from 'fs';
+import path from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dirToCopy = path.join(__dirname, 'files');
+const copyDest = path.join(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+  fs.access(dirToCopy, fs.F_OK, err => {
+    if (err) {
+      throw new Error('FS operation failed');
+    }
+  });
+
+  fs.access(copyDest, fs.F_OK, err => {
+    if (err) return;
+    throw new Error('FS operation failed');
+  });
+
+  fs.cp(dirToCopy, copyDest, { recursive: true }, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
 
 await copy();
