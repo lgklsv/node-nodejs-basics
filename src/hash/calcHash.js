c8f2a9bcd66f3068b2fad3c37e72db7d49d1c3cd
lgklsv <kolosov20011@gmail.com>
2023-06-17 17:16:28 +0300

    feat: implement function to hash a file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import * as url from 'url';
+import fs from 'fs/promises';
+import path from 'path';
+import crypto from 'crypto';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToHash = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileBuffer = await fs.readFile(fileToHash);
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+
+    const hex = hashSum.digest('hex');
+    console.log(hex);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await calculateHash();
