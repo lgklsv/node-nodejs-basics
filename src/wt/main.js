55293bc32799fddf5831561a09cda9bb8e8357cd
lgklsv <kolosov20011@gmail.com>
2023-06-17 21:21:43 +0300

    feat: inplement worker threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { fileURLToPath } from 'node:url';
+import os from 'node:os';
+import path from 'path';
+import { Worker } from 'node:worker_threads';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerPath = path.join(__dirname, 'worker.js');
+const CPU_CORES = os.cpus().length;
+const DEFAULT_FIB_INPUT = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+  const createWorker = fibInput =>
+    new Promise(resolve => {
+      const worker = new Worker(workerPath, { workerData: { fibInput } });
+      worker.on('message', data => resolve({ status: 'resolved', data }));
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+    });
+
+  const workers = [];
+  for (let i = 0; i < CPU_CORES; i++) {
+    workers.push(createWorker(i + DEFAULT_FIB_INPUT));
+  }
+  const res = await Promise.all(workers);
+  console.log(res);
 };
 
 await performCalculations();
