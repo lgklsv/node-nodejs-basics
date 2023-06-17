f77e4f313d696026851a6d64b1cf7468b2ba69d6
lgklsv <kolosov20011@gmail.com>
2023-06-17 17:35:20 +0300

    feat: read file with streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import * as url from 'url';
+import path from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  fs.createReadStream(fileToRead).pipe(process.stdout);
 };
 
 await read();
