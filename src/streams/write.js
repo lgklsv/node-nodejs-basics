015153473662c892e04be5946e28092eb86b18ca
lgklsv <kolosov20011@gmail.com>
2023-06-17 17:48:58 +0300

    feat: write user input to file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import * as url from 'url';
+import path from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToWrite = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = fs.createWriteStream(fileToWrite);
+  process.stdin.on('data', data => {
+    writeStream.write(data);
+  });
 };
 
 await write();
