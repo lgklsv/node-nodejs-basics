e675440074e724ac487ecd45e3b0ba4800fa97c1
lgklsv <kolosov20011@gmail.com>
2023-06-18 21:11:48 +0300

    feat: spawn child process and establish connection between them

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import * as url from 'url';
+import path from 'path';
+import { fork } from 'node:child_process';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const childPath = path.join(__dirname, 'files', 'script.js');
+
+const spawnChildProcess = async args => {
+  fork(childPath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hi', 'how', 'are', 'you']);
